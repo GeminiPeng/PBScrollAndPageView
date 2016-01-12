541aeb0b7dcf44f654df928861037dcd52b71732
ten-step.pengbo <bo.peng@ten-step.com>
2016-01-12 10:04:58 +0800

    update

---
+++ ScrollAndPageView/PBScrollAndPageView.h
@@ -0,0 +1,19 @@
+//
+//  PBScrollAndPageView.h
+//  ScrollAndPageView
+//
+//  Created by ten-step on 16/1/12.
+//  Copyright © 2016年 ten-step. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@protocol PBScrollViewDelegate;
+@interface PBScrollAndPageView : UIView<UIScrollViewDelegate>
+{
+    __unsafe_unretained id <PBScrollViewDelegate> _delegate;
+}
+@property (nonatomic,assign)id<PBScrollViewDelegate>delegate;
+@property (nonatomic,assign)NSInteger currenPage;
+@property (nonatomic,strong)NSMutableArray *imageArray;
+@property (nonatomic,readonly)UIScrollView *scrollview;
+@end
---
+++ ScrollAndPageView/PBScrollAndPageView.m
@@ -0,0 +1,21 @@
+//
+//  PBScrollAndPageView.m
+//  ScrollAndPageView
+//
+//  Created by ten-step on 16/1/12.
+//  Copyright © 2016年 ten-step. All rights reserved.
+//
+
+#import "PBScrollAndPageView.h"
+
+@implementation PBScrollAndPageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
